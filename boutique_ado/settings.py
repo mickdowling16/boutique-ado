e237b26da42d8399cdf6f6609b7009289b67c80c
mickdowling16 <>
2023-10-21 10:48:37 +0000

    added allowed host

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mickdowling16-boutique-a-ov8mhrocp3.us2.codeanyapp.com', 'boutique-ao-project.herokuapp.com']
+ALLOWED_HOSTS = ['8000-mickdowling16-boutique-a-ov8mhrocp3.us2.codeanyapp.com', 'boutique-ao-project-f0c3831dc4e8.herokuapp.com']
 
 
 # Application definition
